44998d7d0867a20cddfb0dbba1b40afe1447bc69
Zach Phillips <zphillips.de@gmail.com>
2026-04-21 22:05:16 -0400

    01-hello-raylib: raylib window with game loop working

--- 01-hello-raylib/main.cpp
+++ 01-hello-raylib/main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include "raylib.h"
 
 int main() {
-	std::cout << "Hello, Raylib!" << std::endl;
+	InitWindow(800, 600, "Hello Raylib!");
+	SetTargetFPS(60); // Set the target frames-per-second (FPS)
+	while (!WindowShouldClose()) { // Check if the window should close
+		BeginDrawing(); // Start drawing
+		ClearBackground(RAYWHITE); // Clear the background with a white color
+		EndDrawing(); // End drawing
+	}
+	CloseWindow(); // Close the window and OpenGL context
 	return 0;
 }
