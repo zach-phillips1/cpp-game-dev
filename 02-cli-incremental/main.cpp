55bcbd3f013d5f181cd4d409f2eebb6637171544
Zach Phillips <zphillips.de@gmail.com>
2026-04-21 22:51:24 -0400

    02-cli-incremental: switch to C++20, use designated initializers

--- 02-cli-incremental/main.cpp
+++ 02-cli-incremental/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+struct GameState {
+	int trees_available;
+	int logs;
+	int planks;
+	int arborists;
+	int lumberjacks;
+	int sawyers;
+};
+
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	GameState state = {
+		.trees_available = 100,
+		.logs = 50,
+		.planks = 10,
+		.arborists = 5,
+		.lumberjacks = 3,
+		.sawyers = 2
+	};
+	std::cout << "Trees available: " << state.trees_available << std::endl;
+	std::cout << "Logs: " << state.logs << std::endl;
+	std::cout << "Planks: " << state.planks << std::endl;
+	std::cout << "Arborists: " << state.arborists << std::endl;
+	std::cout << "Lumberjacks: " << state.lumberjacks << std::endl;
+	std::cout << "Sawyers: " << state.sawyers << std::endl;
 	return 0;
 }
