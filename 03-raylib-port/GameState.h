8b99011f13916c3dd0243c8e69af41cc3437ef45
Zach Phillips <zphillips.de@gmail.com>
2026-04-25 16:28:33 -0400

    03-raylib-port: created scaffold. Added function declarations to GameState.h.

---
+++ 03-raylib-port/GameState.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+struct GameState {
+    int tick_count;
+    int trees_available;
+    int logs;
+    int planks;
+    int arborists;
+    int lumberjacks;
+    int sawyers;
+};
+
+void tick(GameState& state);
+void saveGame(const GameState& state, const std::string& filename);
+void loadGame(GameState& state, const std::string& filename);
+bool handleInput(GameState& state);
