bf20014580fa114e186b1ae8de246923622277b7
Zach Phillips <zphillips.de@gmail.com>
2026-04-25 12:10:23 -0400

    Added save and load.

--- 02-cli-incremental/main.cpp
+++ 02-cli-incremental/main.cpp
@@ -3,8 +3,10 @@
 #include <chrono>
 #include <thread>
 #include <conio.h>
+#include <fstream>
 
 struct GameState {
+	int tick_count;
 	int trees_available;
 	int logs;
 	int planks;
@@ -12,6 +14,9 @@ struct GameState {
 	int lumberjacks;
 	int sawyers;
 };
+
+void saveGame(const GameState& state, const std::string& filename);
+void loadGame(GameState& state, const std::string& filename);
 
 void tick(GameState& state) {
 	// Simulate the passage of time and update the game state
@@ -60,14 +65,52 @@ void handle_input(GameState& state) {
 					std::cout << "Not enough planks to add a sawyer!" << std::endl;
 				}
 				break;
+			case 'q': // Quit the game
+				std::cout << "Quitting the game..." << std::endl;
+				saveGame(state, "savegame.txt");
+				exit(0);
+				break;
 			default:
 				break;
 		}
 	}
 }
 
+void saveGame(const GameState& state, const std::string& filename) {
+	std::ofstream saveFile(filename);
+	if (saveFile.is_open()) {
+		saveFile << state.trees_available << std::endl;
+		saveFile << state.logs << std::endl;
+		saveFile << state.planks << std::endl;
+		saveFile << state.arborists << std::endl;
+		saveFile << state.lumberjacks << std::endl;
+		saveFile << state.sawyers << std::endl;
+		saveFile.close();
+		std::cout << "Game saved!" << std::endl;
+	} else {
+		std::cout << "Unable to save game!" << std::endl;
+	}
+}
+
+void loadGame(GameState& state, const std::string& filename) {
+	std::ifstream loadFile(filename);
+	if (loadFile.is_open()) {
+		loadFile >> state.trees_available;
+		loadFile >> state.logs;
+		loadFile >> state.planks;
+		loadFile >> state.arborists;
+		loadFile >> state.lumberjacks;
+		loadFile >> state.sawyers;
+		loadFile.close();
+		std::cout << "Game loaded!" << std::endl;
+	} else {
+		std::cout << "Unable to load game!" << std::endl;
+	}
+}
+
 int main() {
 	GameState state = {
+		.tick_count = 0,
 		.trees_available = 0,
 		.logs = 0,
 		.planks = 0,
@@ -76,14 +119,16 @@ int main() {
 		.sawyers = 1
 	};
 
-	auto now = std::chrono::steady_clock::now();
-	int tick_count = 0;
+	loadGame(state, "savegame.txt");
 
 	while (true) {
 		std::this_thread::sleep_for(std::chrono::seconds(1)); // Simulate time passing
 		tick(state);
-		handle_input(state);
 		tick_count++;
+		if (tick_count % 60 == 0) {
+			saveGame(state, "savegame.txt");
+		}
+		handle_input(state);
 		std::cout << "Tick: " << tick_count << " | Trees: " << state.trees_available
 				  << " | Logs: " << state.logs
 			<< " | Planks: " << state.planks << std::endl;
